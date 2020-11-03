06c12887ef2fb42e04460c39847b217bbef1849d
Hein Bo <Heinbo@outlook.com>
2020-11-03 16:05:31 +0100

    Add project files.

---
+++ Datentypen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Datentypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long Ganzzahl64Bit; // -9 Trillionen bis +9 Trillionen
+            ulong Ganzzahl64Bit; // 0 bis +18 Trillionen
+
+            int Ganzzahl32Bit; // -2,14 Milliarden bis + 2,14 Milliarden
+            uint Ganzzahl32BitPositiv; // 0 bis +4,29 Milliarden
+
+            short Ganzzahl16Bit; // -32 tausend bis +32 tausend
+            ushort Ganzzahl16Bit; // 0 bis 65 tausend
+
+            sbyte Ganzzahl8Bit; // -128 bis +127
+            byte GanzzahlPositiv; // 0 bis 255
+
+            /////////////////////////////////////////////////////////////
+
+            float Gleitkommazahl32Bit; // ca 6 Stellen genau
+            double Gleitkommazahl64Bit; // ca 15 Stellen genau
+            decimal Gleitkommazahl128Bit; // ca 30 Stellen genau
+
+            ////////////////////////////////////////////////////////////
+
+            bool JaOderNein; // exakt zwei Werte. true (1) false (0)
+
+            ////////////////////////////////////////////////////////////
+
+            byte AktiveOptionen = (byte)(MyEnum.A | MyEnum.D);
+
+
+
+
+            Console.WriteLine("Hello World!");
+        } // ende Main
+        
+        enum MyEnum
+        {
+            A = 1,
+            B = A*2,
+            C = B*2,
+            D = C*2
+        }
+    }
+}
---
+++ Kontrollstrukturen/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kontrollstrukturen
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Bedingungen
+
+
+            if (1 > 2) // testet einen ausdruck ob er TRUE oder FALSE ist
+            {
+                // wenn 1 grösser ist als 2
+            }
+
+            if (1 < 2)
+            {
+                // wenn 1 kleiner als 2
+            }
+            else
+            {
+                // in jedem anderen fall
+            }
+
+            if (1 < 2 && 3 > 2) // und verknüpfung
+            {
+                // wenn beide bedingungen erfüllt sind
+            }
+
+            if (1 < 2 || 3 > 2) // oder verknüpfung
+            {
+
+            }
+
+            if (1 < 2)
+            {
+                if (3 > 2)
+                {
+                    // wenn beide bedingungen erfüllt sind
+                }
+                else
+                {
+                    // erste erfüllt, zweite aber nicht
+                }
+            }
+            else
+            {
+                // erste bereits gescheitert
+            }
+
+            if (3 > 2 ||
+                (1 < 3 && 2 == 2))
+            {
+
+            }
+
+            int VariableA;
+
+            //          ( bedingung     ? true : false)
+            VariableA = (3 < 2 || 5 > 3 ? 5 : 3);
+            #endregion
+
+            #region Schleifen
+
+            int Anfang = 0;
+
+            while (Anfang < 10)
+            {
+                Anfang++;
+            }
+
+            do
+            {
+                Anfang--;
+            } while (Anfang > 0); //wanna see me do it again?
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+            }
+
+
+            for (; ; Anfang++)// die bereiche des for sind alle optional for(;;) ist gültig
+            {
+                if (Anfang > 20)
+                {
+                    break;
+                }
+            }
+
+            List<int> ListOfNumbers = new List<int>();
+
+            foreach (int item in ListOfNumbers)
+            {
+
+            }
+
+            #endregion
+        }
+
+    }
+}
